a4ba9734e9c06610195d0d2f61e0940992aeae35
Iao Seng Sio <tonysio42@gmail.com>
2017-04-08 17:33:24 -1000

    Removed unnecessary import

--- app/src/main/java/kesira/papertrader/StockInfoActivity.java
+++ app/src/main/java/kesira/papertrader/StockInfoActivity.java
@@ -12,7 +12,6 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
